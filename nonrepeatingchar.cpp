4dab81a7b61237e9ee14956ec0700074705780f9
Ayuti-Pardeshi <162686430+Ayuti-Pardeshi@users.noreply.github.com>
2025-06-18 23:54:53 +0530

    Create nonrepeatingchar.cpp

---
+++ nonrepeatingchar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "aabbcddce";
+    int count[256] = {0};  // for all characters
+
+    // Step 1: Count each character
+    for (int i = 0; i < s.length(); i++) {
+        count[s[i]]++;
+    }
+
+    // Step 2: Find first one that appears only once
+    for (int i = 0; i < s.length(); i++) {
+        if (count[s[i]] == 1) {
+            cout << "First unique character is: " << s[i];
+            return 0;
+        }
+    }
+
+    cout << "No unique character.";
+    return 0;
+}
