b62010e2f66e5aab49dda2029f241213b3accf76
Ayuti-Pardeshi <162686430+Ayuti-Pardeshi@users.noreply.github.com>
2025-06-25 23:58:12 +0530

    Create rightshiftfastdivision.cpp

---
+++ rightshiftfastdivision.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 40;
+
+    cout << "Original number: " << n << endl;
+    cout << "Right shift by 3 (n >> 3): " << (n >> 3) << endl;
+
+    return 0;
+}
