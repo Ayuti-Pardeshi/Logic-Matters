9f058bb2818b53450b72f698b7cc4f100b2b6fe7
Ayuti-Pardeshi <162686430+Ayuti-Pardeshi@users.noreply.github.com>
2025-06-25 23:57:46 +0530

    Create BitwiseFastMultiplication.cpp

---
+++ BitwiseFastMultiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 7;
+
+    cout << "Original number: " << n << endl;
+    cout << "Left shift by 2 (n << 2): " << (n << 2) << endl;
+
+    return 0;
+}
